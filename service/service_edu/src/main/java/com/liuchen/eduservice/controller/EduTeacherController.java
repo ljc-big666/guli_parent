ff55cb93f72f7b677ef08b174895355888745235
LiuChen <2075451624@qq.com>
2022-10-17 11:14:20 +0800

    Test02

--- service/service_edu/src/main/java/com/liuchen/eduservice/controller/EduTeacherController.java
+++ service/service_edu/src/main/java/com/liuchen/eduservice/controller/EduTeacherController.java
@@ -1,16 +1,22 @@
 package com.liuchen.eduservice.controller;
 
 
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.liuchen.commonutils.R;
 import com.liuchen.eduservice.entity.EduTeacher;
+import com.liuchen.eduservice.entity.vo.TeacherQuery;
 import com.liuchen.eduservice.service.EduTeacherService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiParam;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  * <p>
@@ -50,6 +56,68 @@ public class EduTeacherController {
         } else {
             return R.error();
         }
+    }
+
+    // 3、分页查询讲师的方法
+    @ApiOperation(value = "分页查询")
+    @GetMapping("pageTeacher/{current}/{limit}")
+    public R pageListTeacher(@PathVariable long current,
+                             @PathVariable long limit){
+        // 创建page对象
+        Page<EduTeacher> pageTeacher = new Page<>(current,limit);
+        // 调用方法实现分页
+        // 调用方法时候，底层封装，把分页所有数据封装到pageTeacher对象里面
+        eduTeacherService.page(pageTeacher, null);
+
+        long total = pageTeacher.getTotal();// 总记录数
+        List<EduTeacher> records = pageTeacher.getRecords();// 数据list集合
+
+        /*Map map = new HashMap();
+        map.put("total",total);
+        map.put("rows",records);
+        return R.ok().data(map);*/
+
+        return R.ok().data("total",total).data("rows",records);
+    }
+
+    // 4、条件查询带分页的方法
+    @ApiOperation(value = "条件查询带分页的方法")
+    @PostMapping("pageTeacherCondition/{current}/{limit}")
+    public R pageTeacherCondition(@PathVariable long current,
+                                  @PathVariable long limit,
+                                  @RequestBody(required = false) TeacherQuery teacherQuery){
+        // 创建page对象
+        Page<EduTeacher> pageTeacher = new Page<>();
+
+        // 构造条件
+        QueryWrapper<EduTeacher> wrapper = new QueryWrapper<>();
+        String name = teacherQuery.getName();
+        Integer level = teacherQuery.getLevel();
+        String begin = teacherQuery.getBegin();
+        String end = teacherQuery.getEnd();
+        // 判断条件值是否为空，如果不为空拼接条件
+        if(!StringUtils.isEmpty(name)){
+            // 构造条件
+            wrapper.like("name",name);
+        }
+        if(!StringUtils.isEmpty(level)){
+            wrapper.eq("level",level);
+        }
+        if(!StringUtils.isEmpty(begin)) {
+            wrapper.ge("gmt_create",begin);
+        }
+        if(!StringUtils.isEmpty(end)) {
+            wrapper.le("gmt_modified",end);
+        }
+
+
+        // 调用方法实现条件查询分页
+        eduTeacherService.page(pageTeacher,wrapper);
+
+        long total = pageTeacher.getTotal();// 总记录数
+        List<EduTeacher> records = pageTeacher.getRecords();// 数据list集合
+
+        return R.ok().data("total",total).data("rows",records);
     }
 }
 
